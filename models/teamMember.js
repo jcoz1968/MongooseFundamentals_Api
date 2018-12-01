a482bb67977e42a5c9db2ec6433c414de2710c09
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-01 17:45:02 -0600

    custom validators

--- models/teamMember.js
+++ models/teamMember.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 
+const sizeValidator = [
+    function(val) {
+        let testVal = val.trim();
+        return (testVal.length > 0 && testVal.length <= 25);
+    },
+    'Please supply a value for {PATH}'
+];
+
 const teamMemberSchema = new mongoose.Schema({
-    name: { type: String }
+    name: { type: String, required: true, validator: sizeValidator }
 });
 
 module.exports = mongoose.model('TeamMember', teamMemberSchema);
